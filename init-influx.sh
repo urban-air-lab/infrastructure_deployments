8107ec55f92014642edd4869181398e38fa9c278
Tobias Weiland <post@tobias-weiland.eu>
2025-04-18 12:17:05 +0200

    add route for measurement bucket

--- init-influx.sh
+++ init-influx.sh
@@ -2,6 +2,6 @@
 set -e
 
 influx bucket create -n UALMinuteCalibrationBucket --org urban-air-lab --token s-0ZETVqp_AT_sWMnRwfSBaG9_vbUvicrWzJJSv_IsCeIg8OGuIQXoTNnbNdRNb_yVKm-PKmZtE246PrQ8e8Rw==
-influx bucket create -n UALMinuteMeasurmentBucket --org urban-air-lab --token s-0ZETVqp_AT_sWMnRwfSBaG9_vbUvicrWzJJSv_IsCeIg8OGuIQXoTNnbNdRNb_yVKm-PKmZtE246PrQ8e8Rw==
+influx bucket create -n UALMinuteMeasurementBucket --org urban-air-lab --token s-0ZETVqp_AT_sWMnRwfSBaG9_vbUvicrWzJJSv_IsCeIg8OGuIQXoTNnbNdRNb_yVKm-PKmZtE246PrQ8e8Rw==
 influx bucket create -n LUBWHourBucket --org urban-air-lab --token s-0ZETVqp_AT_sWMnRwfSBaG9_vbUvicrWzJJSv_IsCeIg8OGuIQXoTNnbNdRNb_yVKm-PKmZtE246PrQ8e8Rw==
 influx bucket create -n LUBWMinuteBucket --org urban-air-lab --token s-0ZETVqp_AT_sWMnRwfSBaG9_vbUvicrWzJJSv_IsCeIg8OGuIQXoTNnbNdRNb_yVKm-PKmZtE246PrQ8e8Rw==
