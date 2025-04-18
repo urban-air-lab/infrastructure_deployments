b323d5567b82d616a163a7553d4640eebb23bd00
Tobias Weiland <post@tobias-weiland.eu>
2025-04-18 09:36:23 +0200

    change organisation to urban-air-lab

--- init-influx.sh
+++ init-influx.sh
@@ -3,7 +3,7 @@ set -e
 
 sleep 5
 
-influx bucket create -n UALMinuteCalibrationBucket --org some_org --token s-0ZETVqp_AT_sWMnRwfSBaG9_vbUvicrWzJJSv_IsCeIg8OGuIQXoTNnbNdRNb_yVKm-PKmZtE246PrQ8e8Rw==
-influx bucket create -n UALMinuteMeasurmentBucket --org some_org --token s-0ZETVqp_AT_sWMnRwfSBaG9_vbUvicrWzJJSv_IsCeIg8OGuIQXoTNnbNdRNb_yVKm-PKmZtE246PrQ8e8Rw==
-influx bucket create -n LUBWHourBucket --org some_org --token s-0ZETVqp_AT_sWMnRwfSBaG9_vbUvicrWzJJSv_IsCeIg8OGuIQXoTNnbNdRNb_yVKm-PKmZtE246PrQ8e8Rw==
-influx bucket create -n LUBWMinuteBucket --org some_org --token s-0ZETVqp_AT_sWMnRwfSBaG9_vbUvicrWzJJSv_IsCeIg8OGuIQXoTNnbNdRNb_yVKm-PKmZtE246PrQ8e8Rw==
+influx bucket create -n UALMinuteCalibrationBucket --org urban-air-lab --token s-0ZETVqp_AT_sWMnRwfSBaG9_vbUvicrWzJJSv_IsCeIg8OGuIQXoTNnbNdRNb_yVKm-PKmZtE246PrQ8e8Rw==
+influx bucket create -n UALMinuteMeasurmentBucket --org urban-air-lab --token s-0ZETVqp_AT_sWMnRwfSBaG9_vbUvicrWzJJSv_IsCeIg8OGuIQXoTNnbNdRNb_yVKm-PKmZtE246PrQ8e8Rw==
+influx bucket create -n LUBWHourBucket --org urban-air-lab --token s-0ZETVqp_AT_sWMnRwfSBaG9_vbUvicrWzJJSv_IsCeIg8OGuIQXoTNnbNdRNb_yVKm-PKmZtE246PrQ8e8Rw==
+influx bucket create -n LUBWMinuteBucket --org urban-air-lab --token s-0ZETVqp_AT_sWMnRwfSBaG9_vbUvicrWzJJSv_IsCeIg8OGuIQXoTNnbNdRNb_yVKm-PKmZtE246PrQ8e8Rw==
