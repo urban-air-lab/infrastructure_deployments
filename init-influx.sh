50df07dbab465543498d53d14989ba889076c691
Tobias Weiland <post@tobias-weiland.eu>
2025-04-18 10:05:46 +0200

    remove sleep from bash file

--- init-influx.sh
+++ init-influx.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-sleep 5
-
 influx bucket create -n UALMinuteCalibrationBucket --org urban-air-lab --token s-0ZETVqp_AT_sWMnRwfSBaG9_vbUvicrWzJJSv_IsCeIg8OGuIQXoTNnbNdRNb_yVKm-PKmZtE246PrQ8e8Rw==
 influx bucket create -n UALMinuteMeasurmentBucket --org urban-air-lab --token s-0ZETVqp_AT_sWMnRwfSBaG9_vbUvicrWzJJSv_IsCeIg8OGuIQXoTNnbNdRNb_yVKm-PKmZtE246PrQ8e8Rw==
 influx bucket create -n LUBWHourBucket --org urban-air-lab --token s-0ZETVqp_AT_sWMnRwfSBaG9_vbUvicrWzJJSv_IsCeIg8OGuIQXoTNnbNdRNb_yVKm-PKmZtE246PrQ8e8Rw==
