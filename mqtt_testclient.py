f80df74fe9035d21f02c40cd22af3433bc3236df
Tobias Weiland <post@tobias-weiland.eu>
2025-04-20 11:47:45 +0200

    refactore testclient setup

--- mqtt_testclient.py
+++ mqtt_testclient.py
@@ -3,9 +3,6 @@ import json
 import paho.mqtt.publish as publish
 import random
 
-BROKER = "localhost"
-PORT = 1883
-TOPIC = "sensors/test-data"
 
 
 def get_sensor_data():
@@ -15,21 +12,23 @@ def get_sensor_data():
     }
 
 
-def publish_sensor_data():
+def publish_sensor_data(topic:str) -> None:
     data = get_sensor_data()
     payload = json.dumps(data)
 
     publish.single(
-        topic=TOPIC,
+        topic=topic,
         payload=payload,
-        hostname=BROKER,
-        port=PORT
+        hostname="localhost",
+        port=1883
     )
 
-    print(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] Published to {TOPIC}: {payload}")
+    print(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] Published to {topic}: {payload}")
 
 
 if __name__ == "__main__":
     while True:
-        publish_sensor_data()
+        publish_sensor_data("sensors/test-data")
+        publish_sensor_data("sensors/calibration")
+        publish_sensor_data("sensors/measurement")
         time.sleep(60)
