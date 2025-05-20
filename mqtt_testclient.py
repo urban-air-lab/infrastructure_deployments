e18425ea024d40acf6e60592df26540b704db8fd
tobias.weiland <tobias.weiland@7p-group.com>
2025-05-20 14:47:20 +0200

    update sensor names in testclient

--- mqtt_testclient.py
+++ mqtt_testclient.py
@@ -29,8 +29,8 @@ def publish_sensor_data(topic:str) -> None:
 if __name__ == "__main__":
     while True:
         publish_sensor_data("sensors/test-data/test")
-        publish_sensor_data("sensors/calibration/sontc")
-        publish_sensor_data("sensors/calibration/sontd")
-        publish_sensor_data("sensors/measurement/sonta")
-        publish_sensor_data("sensors/measurement/sontb")
+        publish_sensor_data("sensors/calibration/ual-1")
+        publish_sensor_data("sensors/calibration/ual-2")
+        publish_sensor_data("sensors/measurement/ual-1")
+        publish_sensor_data("sensors/measurement/ual-2")
         time.sleep(60)
