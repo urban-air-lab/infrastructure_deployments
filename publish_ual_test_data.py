5c370c1661a6a7412c7c1e46a84831a125f38a86
Tobias Weiland <post@tobias-weiland.eu>
2025-11-22 21:00:09 +0100

    switch to uv and ual common lib

--- publish_ual_test_data.py
+++ publish_ual_test_data.py
@@ -1,38 +1,21 @@
 import os
 import time
-import json
-import paho.mqtt.publish as publish
 import random
 from dotenv import load_dotenv
-import pandas as pd
+from ual.mqtt.mqtt_client import MQTTClient
 
 load_dotenv()
 
-
-def get_sensor_data():
-    return pd.DataFrame(data={
-        "temperature": [random.randint(15, 25), random.randint(15, 25), random.randint(15, 25)],
-        "humidity": [random.randint(30, 60), random.randint(30, 60), random.randint(30, 60)],
-        "timestamp": [time.time(), time.time(), time.time()]
-    }, index=[0, 1, 3])
-
-
-def publish_sensor_data(topic: str) -> None:
-    data = get_sensor_data()
-    payload = data.to_json()
-
-    publish.single(
-        topic=topic,
-        payload=payload,
-        hostname=os.getenv("MQTT_SERVER"),
-        port=int(os.getenv("MQTT_PORT")),
-        auth={'username': os.getenv("MQTT_USERNAME"), 'password': os.getenv("MQTT_PASSWORD")}
-    )
-
-    print(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] Published to {topic}: {payload}")
-
+def get_sensor_data() -> dict:
+    return {
+        "temperature": random.randint(15, 25),
+        "humidity": random.randint(30, 60),
+        "timestamp": time.time()
+    }
 
 if __name__ == "__main__":
+    mqtt_client = MQTTClient(os.getenv("MQTT_SERVER"), int(os.getenv("MQTT_PORT")), os.getenv("MQTT_USERNAME"), os.getenv("MQTT_PASSWORD"))
     while True:
-        publish_sensor_data("sensors/test-data/test")
+        data = get_sensor_data()
+        mqtt_client.publish_data(data,"sensors/test-data/test" )
         time.sleep(60)
